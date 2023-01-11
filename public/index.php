c244c5161534404bc95086f1f556259a4a059aac
shano1993 <stefan.hanotiau@gmail.com>
2023-01-11 11:49:51 +0100

    finish

---
+++ public/index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Notes</title>
+    <link rel="stylesheet" href="assets/style.css">
+</head>
+<body>
+    <div id="container">
+        <h1>Une petite note ?</h1>
+        <textarea name="note" id="note" cols="75" rows="40">
+
+        </textarea>
+    </div>
+</body>
+</html>
